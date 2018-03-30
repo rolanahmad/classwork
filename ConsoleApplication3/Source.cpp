165a5f8b29b06d34561866744d2b13c5265ecbd2
ГерценЕ <@403_TEACHER>
2018-03-30 09:51:11 +0600

    -

--- ConsoleApplication3/Source.cpp
+++ ConsoleApplication3/Source.cpp
@@ -4,21 +4,104 @@
 using namespace std;
 
 /*
-—формировать массив структур, каждый элемент которого содержит следующие пол€ :
+—формировать массив структур, каждый элемент которого содержит следующие три пол€:
 
-шифр подразделени€, содержащий не более трЄх символов(вводим);
+фамили€, им€ отчество студента (вводим);
 
-план выпуска продукции по кварталам одного года в виде массива(вводим);
+массив оценок в дес€тибалльной системе (вводим);
 
-фактическое выполнение плана по кварталам одного года в виде массива(вводим);
+признак
 
 
 
-–ассортировать массив структур следующим образом : сначала должна быть информаци€ о тех подразделени€х,
-которые выполнили план во всех кварталах, затем о тех, кто выполнил план в трЄх кварталах, и т.д.
+ƒл€ каждого студента получить одно из следующих значений признака: 
+5 Ц отличник (только 9 и (или) 10),
+4 Ц хорошист (нет оценок, меньше 6, но не все 9 или 10, а есть 6 и (или) 7 и (или) 8),
+3 Ц троечник (нет оценок, меньше 4, но есть 4 и (или) 5),
+2 Ц неуспевающий (есть 1 и (или) 2 и (или) 3). 
+–ассортировать массив структур по убыванию полученного признака.
 */
+
+struct fio {
+	char *surname;
+	char *name;
+	char *patronymic;
+};
+
+struct student {
+	fio *fio;
+	int *assessments;
+	int attribute;
+	int sum;
+};
+
+void generate_name(fio *fio);
 
 
 void main() {
+	srand(time(0));
+	rand();
+	int students_colvo;
+	cout << "ѕожалуйста введите количество студентов: ";
+	cin >> students_colvo;
+	student *students = new student[students_colvo];
+	if (students == NULL) {
+		cout << "ѕам€ть не выделенна" << endl;
+	}
+	else {
+		for (int i = 0; i < students_colvo; i++)
+		{
+			generate_name(students->fio);
+
+			students->assessments = new int[10];
+			for (int j = 0; j < 10; j++)
+				students->assessments[j] = 1 + rand() % 9;
+				
+			students++;
+		}
+		students -= students_colvo;
+
+	}
+}
+/*
+5 Ц отличник(только 9 и(или) 10),
+4 Ц хорошист(нет оценок, меньше 6, но не все 9 или 10, а есть 6 и(или) 7 и(или) 8),
+3 Ц троечник(нет оценок, меньше 4, но есть 4 и(или) 5),
+2 Ц неуспевающий(есть 1 и(или) 2 и(или) 3).
+*/
+void chek_assessments(student *student) {
+	if (student->sum > 8) {
+		student->attribute = 5;
+	}
+	else if (student->sum > 6 && student->sum < 9) {
+		student->attribute = 4;
+	}
+	else if (student->sum > 4 && student->sum < 8) {
+		student->attribute = 3;
+	}
+	else {
+		student->attribute = 2;
+	}
+
+
+}
+
+void calculate_assessments(student *student) {
+	for (int i = 0; i < 10; i++) 
+		student->sum += student->assessments[i];
+	
+	student->sum = student->sum / 10;
+}
+
+void generate_name(fio *fio) {
+	fio->name = new char[3];
+	fio->patronymic = new char[3];
+	fio->surname = new char[3];
+	for (int i = 0; i < 3; i++)
+	{
+		fio->name[i] = 97 + rand() % 95;
+		fio->patronymic[i] = 97 + rand() % 95;
+		fio->surname[i] = 97 + rand() % 95;
+	}
 
 }
